25313d9f42391c8ad2127414037ad738f3131711
Dunlap <cdunla6940@greenville.k12.sc.us>
2017-09-22 14:04:48 -0400

    Made some finishing touches

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,18 +1,30 @@
 import random
+import math
 
 # config
 low = 1
 high = 100
-limit = 10
+limit = math.ceil(math.log(100, 2))
 
 # helper functions
 def show_start_screen():
-    print("**************************")
-    print("**** Guess a Number ! ****")
-    print("**************************")
+    print(""" _____                               _   _                 _               
+|  __ \                             | \ | |               | |              
+| |  \/_   _  ___  ___ ___    __ _  |  \| |_   _ _ __ ___ | |__   ___ _ __ 
+| | __| | | |/ _ \/ __/ __|  / _` | | . ` | | | | '_ ` _ \| '_ \ / _ \ '__|
+| |_\ \ |_| |  __/\__ \__ \ | (_| | | |\  | |_| | | | | | | |_) |  __/ |   
+ \____/\__,_|\___||___/___/  \__,_| \_| \_/\__,_|_| |_| |_|_.__/ \___|_|   """)
+
 
 def show_credits():
-    print("This awesome game was created by a small 10th grader.")
+    print("""___  ___          _       ______         _____ _                     ______             _             
+|  \/  |         | |      | ___ \       /  __ \ |                    |  _  \           | |            
+| .  . | __ _  __| | ___  | |_/ /_   _  | /  \/ |__   __ _ ___  ___  | | | |_   _ _ __ | | __ _ _ __  
+| |\/| |/ _` |/ _` |/ _ \ | ___ \ | | | | |   | '_ \ / _` / __|/ _ \ | | | | | | | '_ \| |/ _` | '_ \ 
+| |  | | (_| | (_| |  __/ | |_/ / |_| | | \__/\ | | | (_| \__ \  __/ | |/ /| |_| | | | | | (_| | |_) |
+\_|  |_/\__,_|\__,_|\___| \____/ \__, |  \____/_| |_|\__,_|___/\___| |___/  \__,_|_| |_|_|\__,_| .__/ 
+                                  __/ |                                                        | |    
+                                 |___/                                                         |_|""")
     
 def get_guess():
     while True:
@@ -25,25 +37,33 @@ def get_guess():
             print("You must enter a positive number you nimrod.")
 
 def pick_number():
-    print("I'm thinking of a number from " + str(low) + " to " + str(high) +".")
+    print(" ")
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) +". You have " + str(limit) + " tries.")
 
     return random.randint(low, high)
 
 def check_guess(guess, rand):
     if guess < rand:
+        print(" ")
         print("You guessed too low.")
     elif guess > rand:
+        print(" ")
         print("You guessed too high.")
 
 def show_result(guess, rand):
     if guess == rand:
+        print(" ")
         print("You win! now go get a life.")
+        print(" ")
     else:
+        print(" ")
         print("Wow you're really jsut a waste in space aren't you? The number was " + str(rand) + ".")
+        print(" ")
 
 def play_again():
     while True:
         decision = input("Would you like to play again? (y/n) ")
+        decision = decision.lower()
 
         if decision == 'y' or decision == 'yes':
             return True
@@ -51,6 +71,7 @@ def play_again():
             return False
         else:
             print("Wow you can't even type y or n.")
+            print(" ")
 
 def play():
     guess = -1
